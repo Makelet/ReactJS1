d6080f1d8fc0e1d9534a441e2d5192d35e098e5e
Makelet <smsaud3638@gmail.com>
2024-04-24 12:14:39 +0530

    React JS Project Begins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [value, setValue] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {value}
+      </h1>
+      <button onClick={() => { setValue(value + 1) }}>Click me</button>
     </div>
   );
 }
