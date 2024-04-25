f78ab2fb7ba6b28515b7666ee7fc0b0fc6cba7be
Makelet <smsaud3638@gmail.com>
2024-04-25 16:01:51 +0530

    useRef() hook is used in this commit

--- src/CodePractice/App_1.js
+++ src/CodePractice/App_1.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { useState, useEffect, useRef } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -16,34 +16,33 @@ function App() {
 
   let [color, setColor] = useState(0);
 
-  // let n1 = 0; 
-  // const n1 = useRef(0);
-  const btnRef = useRef(null);
-
-  useEffect(() => {
-    btnRef.current.style.backgroundColor = "red"
-    // n1.current = n1.current + 1;
-    // console.log(`The value of n1 is ${n1.current}`);
-    console.log(btnRef);
-  });
+  // useEffect(() => {
+  //   alert("Run on Every Render");
+  // })
+  // useEffect(() => {
+  //   alert("Run on Only First Render");
+  // }, []);
+  // useEffect(() => {
+  //   alert("Count was changed");
+  //   setColor(color + 1);
+  // }, [count]);
+  // useEffect(() => {
+  //   alert("Color was changed");
+  // }, [color]);
 
   return (
     <>
       <NavBar />
       <h1>{count}</h1>
-      <button ref={btnRef} onClick={() => { setCount(count + 1) }}>Click Me</button>
+      <button onClick={() => { setCount(count + 1) }}>Click Me</button>
       <main className="cards">
         <Card title="Project1" desc="Desc1" image={bg1} />
         <Card title="Project2" desc="Desc2" image={bg2} />
         <Card title="Project3" desc="Desc3" image={bg3} />
         <Card title="Project4" desc="Desc4" image={bg4} />
       </main>
-
-      <button onClick={()=>{btnRef.current.style.backgroundColor="green"}}>Change Color</button>
-      {/* <button onClick={()=>{btnRef.current.style.backgroundColor="green"}}>Change Color</button> */}
       <Footer />
     </>
   );
 }
 
-export default App;
